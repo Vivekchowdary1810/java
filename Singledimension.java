762393775841b1987bffad0f1a1ec2f28b53d212
Vivekchowdary1810 <vivekchowdary213@gmail.com>
2025-05-03 20:41:32 +0530

    Add files via upload

---
+++ Singledimension.java
@@ -0,0 +1,17 @@
+public class Singledimension {
+    
+        public static void main(String[] args){
+            int arr[]=new int[5];
+            arr[0]=10;
+            arr[1]=5;
+            arr[2]=6;
+            arr[3]=20;
+            arr[4]=30;
+            for(int result:arr){
+                System.out.println(result+" ");
+            }
+            }
+            
+    }
+    
+
