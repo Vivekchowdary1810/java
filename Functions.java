0eeeb94f82eef351b0c55893746c265c20b5e8a7
Vivekchowdary1810 <vivekchowdary213@gmail.com>
2025-05-05 22:31:56 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,11 @@
+public class Functions {
+    public static void fun(String s,int n){
+        for(int i=0;i<n;i++){
+        System.out.println(s);
+        }
+    }
+    public static void main(String args[]){
+        fun("Rishabh",5);
+        fun("Cricket",3);  
+    }
+}
