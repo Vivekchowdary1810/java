71d5076f8f4f9b99862847afaa33dd4abca265a2
Vivekchowdary1810 <vivekchowdary213@gmail.com>
2025-05-20 16:09:34 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        System.out.println("connection established");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter numerator");
+        int a=scan.nextInt();
+        System.out.println("Enter denominator");
+        int b=scan.nextInt();
+        int c=a/b;
+        System.out.println("size of an array");
+        int size=scan.nextInt();
+         System.out.println("Enter index");
+        int index=scan.nextInt();
+         System.out.println(" Enter element");
+        int element=scan.nextInt();
+      
+        System.out.println("connection terminated");
+        
+    }
+    
+}
